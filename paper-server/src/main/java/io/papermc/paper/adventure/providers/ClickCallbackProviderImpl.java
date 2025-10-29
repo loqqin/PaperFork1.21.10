6731e0258065c0730eccb1cf64adff6cbf1d77d5
loqqin <soubmatic@gmail.com>
2025-10-30 00:16:03 +0200

    loqqin

--- paper-server/src/main/java/io/papermc/paper/adventure/providers/ClickCallbackProviderImpl.java
+++ paper-server/src/main/java/io/papermc/paper/adventure/providers/ClickCallbackProviderImpl.java
@@ -79,7 +79,7 @@ public class ClickCallbackProviderImpl implements ClickCallback.Provider {
                     this.tryConsumeCallback(id.get(), callback -> {
                         callback.accept(PaperDialogResponseView.createUnvalidatedResponse(t), audience);
                     });
-                } catch (Exception e) {
+                } catch (Throwable e) {
                     e.printStackTrace();
                 }
             });
@@ -120,10 +120,14 @@ public class ClickCallbackProviderImpl implements ClickCallback.Provider {
         }
 
         final void tryConsumeCallback(final I key, final Consumer<? super C> callbackConsumer) {
-            final StoredCallback<C, I> callback = this.callbacks.get(key);
-            if (callback != null && callback.valid()) {
-                callback.takeUse();
-                callbackConsumer.accept(callback.callback);
+            try {
+                final StoredCallback<C, I> callback = this.callbacks.get(key);
+                if (callback != null && callback.valid()) {
+                    callback.takeUse();
+                    callbackConsumer.accept(callback.callback);
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
             }
         }
 
