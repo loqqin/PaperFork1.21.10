e0886e7fe0c3a8a8545b65913e9197667d1e223e
aaa <121246396+loqqin@users.noreply.github.com>
2026-06-25 22:23:49 +0300

    loqqin

--- test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPlugin.java
@@ -1,34 +1,36 @@
 package io.papermc.testplugin;
 
-import net.minecraft.server.network.ServerLoginPacketListenerImpl;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.block.Biome;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.SplashPotion;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.generator.BiomeProvider;
-import org.bukkit.generator.ChunkGenerator;
-import org.bukkit.generator.WorldInfo;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import java.util.List;
-import java.util.Random;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
-    public static TestPlugin INSTANCE;
+  public static TestPlugin INSTANCE;
 
-    @Override
-    public void onEnable() {
-        INSTANCE = this;
-        this.getServer().getPluginManager().registerEvents(this, this);
-        System.out.println("ServerLoginPacketListenerImpl.disguiseFromTo = " + (ServerLoginPacketListenerImpl.disguiseFromTo));
+  @Override
+  public void onEnable() {
+    INSTANCE = this;
+    this.getServer().getPluginManager().registerEvents(this, this);
 
-        // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
-    }
+    // io.papermc.testplugin.brigtests.Registration.registerViaOnEnable(this);
+  }
+
+  @EventHandler
+  public void interact(PlayerInteractEvent event) {
+    Player player = event.getPlayer();
+    PlayerInventory inventory = player.getInventory();
+    ItemStack itemInMainHand = inventory.getItemInMainHand();
+    itemInMainHand.setAmount(itemInMainHand.getAmount() - 1);
+    inventory.setItemInMainHand(itemInMainHand);
+  }
+
+  @EventHandler
+  public void onBlockPlace(BlockPlaceEvent event) {
+    event.setCancelled(true);
+  }
 }
